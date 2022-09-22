b613fae6b89d1beac279f54e997a5e1b944e82cd
Tomasz Murzynski <tomasz.murzynski@gmail.com>
2022-09-22 23:09:12 +0200

    Login - phone

--- app/src/main/java/pl/ecokorporacja/igrzyska2023/MainActivity.kt
+++ app/src/main/java/pl/ecokorporacja/igrzyska2023/MainActivity.kt
@@ -1,11 +1,26 @@
 package pl.ecokorporacja.igrzyska2023
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signOutBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        signOutBtn = findViewById(R.id.signOutBtn)
+
+        signOutBtn.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, PhoneActivity::class.java))
+        }
     }
 }
